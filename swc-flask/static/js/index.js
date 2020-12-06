6debc89380d61721c66417752f4590fc0c0cf910
mukanov8 <mukanov.ayan@gmail.com>
2020-12-06 22:47:50 +0900

    Update index.js

--- swc-flask/static/js/index.js
+++ swc-flask/static/js/index.js
@@ -199,6 +199,13 @@ $( document ).ready(function() {
     
     //main function for generating the word cloud
     const generateCloud = (wordCloudParams) => {
+      
+      function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+      }
+
       anychart.onDocumentReady(function () {
 
       if (wordCloudParams.placeholderFlag) text2 = wordCloudParams.placeholderText;
